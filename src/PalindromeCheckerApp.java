088cebfa90cc7765c72bd17c365c860201c335c8
sk5006-dev <sk5006@srmist.edu.in>
2026-03-10 13:57:44 +0530

    UC12: Implement Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,77 @@
+import java.util.*;
+
+interface PalindromeStrategy {
+    boolean isPalindrome(String text);
+}
+
+class StackStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String text) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : text.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : text.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean isPalindrome(String text) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : text.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+class PalindromeChecker {
+
+    private PalindromeStrategy strategy;
+
+    public PalindromeChecker(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String text) {
+        return strategy.isPalindrome(text);
+    }
+}
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String word = "madam";
-        String reversed = "";
+        PalindromeChecker checker =
+                new PalindromeChecker(new StackStrategy());
 
-        for(int i = word.length()-1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
+        System.out.println("Using Stack Strategy:");
+        System.out.println(checker.check("madam"));
 
-        if(word.equals(reversed)) {
-            System.out.println(word + " is a Palindrome");
-        } else {
-            System.out.println(word + " is NOT a Palindrome");
-        }
+        checker.setStrategy(new DequeStrategy());
+
+        System.out.println("Using Deque Strategy:");
+        System.out.println(checker.check("racecar"));
     }
 }
